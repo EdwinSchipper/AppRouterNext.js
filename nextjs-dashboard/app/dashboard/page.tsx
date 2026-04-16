1ce694999cf31573f6345a146f4a77532199b12a
EdwinSchipper <edwin.schipper@gmail.com>
2026-04-16 10:26:43 +0200

    Dashboard Layout

---
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return <p>Dashboard Page</p>;
+}
+
+// const Dashboard = () => {
+//   return (
+//     <div>
+//       <h1>Dashboard</h1>
+//       <p>Welcome to your dashboard!</p>
+//     </div>
+//   );
+// };
+
+// export default Dashboard;
